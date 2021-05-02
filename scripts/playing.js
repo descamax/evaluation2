29577d332deeda2722fb14986dbafdd500cce008
Maxence Descamps <descamax@gmail.com>
2021-05-02 10:12:07 +0200

    disparition des boutons au tour de l'adversaire

--- scripts/playing.js
+++ scripts/playing.js
@@ -25,36 +25,79 @@ $(document).ready(() => {
         return (valeurs[9]);
     }
 
+    function reinitialiserDe(){
+        $('.number2, .number3, .number4, .number5, .number6').css('background-color', 'white').css('font-size', '1em');
+    }
+
+    function reinitialiserAffichage(){
+        $('.cagnotte').text('0 point');
+        cagnotte1 = 0;
+        cagnotte2 = 0;
+       reinitialiserDe();
+    }
+
+
 
     playing1.click(function() {
+        $('.encaisser1').css('display', 'block')
         var jet = play();
         console.log(jet);
+        reinitialiserDe();
         if (jet === 1) {
-            cagnotte1 = 0;
             console.log ('perdu : ' + cagnotte1);
             $('.lancer1').css('display', 'none');
             $('.encaisser1').css('display', 'none');
             $('.lancer2').css('display', 'block');
-            $('.encaisser2').css('display', 'block');
+            reinitialiserAffichage();
         } else {
             cagnotte1 = cagnotte1 + jet;
             console.log ('gagné ! cagnotte1 = ' + cagnotte1);
+            switch (jet) {
+                case 2 :
+                    $('.number2').css('background-color', 'yellow').css('font-size', '1.5em');
+                    break;
+                case 3 :
+                    $('.number3').css('background-color', 'yellow').css('font-size', '1.5em');
+                    break;
+                case 4 :
+                    $('.number4').css('background-color', 'yellow').css('font-size', '1.5em');
+                    break;
+                case 5 :
+                    $('.number5').css('background-color', 'yellow').css('font-size', '1.5em');
+                    break;
+                case 6 :
+                    $('.number6').css('background-color', 'yellow').css('font-size', '1.5em');
+                    break;
+            }
+            if (cagnotte1 === 1) {
+                $('.cagnotte').text(cagnotte1 + ' point');
+            } else {
+                $('.cagnotte').text(cagnotte1 + ' points');
+            }
         }
+        
     })
 
     playing2.click(function() {
+        $('.encaisser2').css('display', 'block')
+
         var jet = play();
         console.log(jet);
         if (jet === 1) {
-            cagnotte2 = 0;
             console.log ('perdu ' + cagnotte2);
             $('.lancer2').css('display', 'none');
             $('.lancer1').css('display', 'block');
             $('.encaisser2').css('display', 'none');
-            $('.encaisser1').css('display', 'block');
+            reinitialiserAffichage();
+
         } else {
             cagnotte2 = cagnotte2 + jet;
             console.log ('gagné ! cagnotte2 = ' + cagnotte2);
+            if (cagnotte1 === 1) {
+                $('.cagnotte').text(cagnotte2 + ' point');
+            } else {
+                $('.cagnotte').text(cagnotte2 + ' points');
+            }
         }
     })
 
@@ -64,22 +107,23 @@ $(document).ready(() => {
         $('.lancer1').css('display', 'none');
         $('.encaisser1').css('display', 'none');
         $('.lancer2').css('display', 'block');
-        $('.encaisser2').css('display', 'block');
-
         console.log('stockage de la cagnotte, score total : ' + score1);
-        cagnotte1 = 0;
+        $('.score1').text(score1);
+        reinitialiserAffichage();
+
+
     })
 
     stock2.click(function() {
         console.log('votre score précédent : ' + score2)
         score2 = score2 + cagnotte2;
         $('.lancer1').css('display', 'block');
-        $('.encaisser1').css('display', 'block');
         $('.lancer2').css('display', 'none');
         $('.encaisser2').css('display', 'none');
         console.log('stockage de la cagnotte, score total = '+ score2);
-        cagnotte2 = 0;
         $('.score2').text(score2);
+        reinitialiserAffichage();
+
     })
 
 })
